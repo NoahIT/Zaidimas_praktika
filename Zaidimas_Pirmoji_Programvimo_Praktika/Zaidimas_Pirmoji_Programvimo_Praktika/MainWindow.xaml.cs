ccc1f7600dbb8596d30c5b46a9cbcf3001381106
Nojus <78691849+NoahIT@users.noreply.github.com>
2023-04-27 18:12:19 +0300

    Add files via upload

---
+++ Zaidimas_Pirmoji_Programvimo_Praktika/Zaidimas_Pirmoji_Programvimo_Praktika/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace Zaidimas_Pirmoji_Programvimo_Praktika
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var ChooseAccountWindow = new ChooseAccountWindow();
+            ChooseAccountWindow.Show();
+            this.Close();
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

ec64c8a0f0977e9a5ed3cc7b495e40226c606d85
Ivan Savochkin <vaniasavochkin@gmail.com>
2023-04-30 18:20:53 +0300

    firstlvlattack

---
+++ Zaidimas_Pirmoji_Programvimo_Praktika/Zaidimas_Pirmoji_Programvimo_Praktika/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zaidimas_Pirmoji_Programvimo_Praktika.Models;
+
+namespace Zaidimas_Pirmoji_Programvimo_Praktika
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap drawSheet;
+        public Entity player;
+        public int k=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            timer1.Enabled = true;
+        }
+
+        public void Init()
+        {
+
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            k++;
+            if (k == 0)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_1.png");
+            if (k == 1)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_2.png");
+            if (k == 2)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_3.png");
+            if (k == 3)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_4.png");
+            if (k == 4)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_5.png");
+            if (k == 5)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_6.png");
+            if (k == 6)
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_7.png");
+            if (k == 7) 
+            {
+                pictureBox1.Image = Image.FromFile("Recourses\\Images\\Ninja\\idle\\Idle_8.png");
+                k = 0; 
+            }
+
+            
+        }
+    }
+}
---
+++ Zaidimas_Pirmoji_Programvimo_Praktika/Zaidimas_Pirmoji_Programvimo_Praktika/Models/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Zaidimas_Pirmoji_Programvimo_Praktika.Models
+{
+    public class Hero : Entity
+    {
+        public Hero(
+            int posX,
+            int posY,
+            int idleFrames,
+            int runFrames,
+            int attackFrames,
+            int deathFrames,
+            Bitmap spriteSheet)
+                        :
+                        base(posX,
+                            posY,
+                            idleFrames,
+                            runFrames,
+                            attackFrames,
+                            deathFrames,
+                            spriteSheet)
+        {
+            
+        }
+    }
+}
